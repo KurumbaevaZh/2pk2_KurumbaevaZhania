35660b1f97664dc2d3ae3c7a4173c444f4efab6a
user <user@DESKTOP-2M6OLN6>
2023-09-09 21:09:58 +0500

    Добавьте файлы проекта.

---
+++ PZ_01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение линейной задачи");
+            //вводим данные
+            double a = 1;
+            double b = Math.PI / 4;
+            double c = 1;
+            //разделим выражение на 3 части
+            double part1 = Math.Pow(Math.Abs(a * Math.Pow(c, 2) - Math.Pow(b, 3)), 1 / 5) + Math.Log(3 * c);
+            double part2 = ((Math.Pow(Math.E, 3 * c)) + Math.Pow(c, 2)) / (Math.Sin(2 * c));
+            double part3 = Math.Pow(10, -3) * Math.Sqrt(2157 * a);
+            //подсчитываем результат 
+            double result = part1 - part2 - part3;
+            //выводим результат
+            Console.WriteLine("Ответ: " + result);
+        }
+    }
+}
