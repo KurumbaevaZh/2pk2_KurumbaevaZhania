5ec5a68ca4dc7d3f3344b240647f5e21cb4138c2
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-09-16 00:06:33 +0500

    добавлена 2 практика

---
+++ PZ_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+namespace PZ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите действительное число");
+            double k = double.Parse(Console.ReadLine());
+            double x, y, t;
+            if (k > 0)
+            {
+                x = Math.Sin(a + (Math.Sqrt(a + k) / 1.5));
+            }
+            else
+                x = Math.Cos(a * k) + 2.5 * a;
+            if (x <= 1) 
+            {
+                y = k + 5 * Math.Cos(Math.Abs(a) * x) + x;
+            }
+            else
+                y = 2 * a * x * x - 3 * k * x;
+            t = 1.3 * x * x + 1.7 * a * a - 3.1 * y * y;
+            Console.WriteLine("Результат" + t);
+        }
+    }
+}
