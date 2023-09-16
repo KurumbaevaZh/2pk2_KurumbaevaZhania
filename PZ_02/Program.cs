231b00988db7cc0a0e8db447a4534658401359f4
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-09-17 00:43:29 +0500

    добавлена 3 практика

--- PZ_02/Program.cs
+++ PZ_02/Program.cs
@@ -6,24 +6,24 @@ namespace PZ_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите целое число");
-            double a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите действительное число");
-            double k = double.Parse(Console.ReadLine());
-            double x, y, t;
-            if (k > 0)
+            Console.WriteLine("Введите целое число");//вводим данные
+            double a = double.Parse(Console.ReadLine());//объявляем переменные
+            Console.WriteLine("Введите действительное число");//вводим данные
+            double k = double.Parse(Console.ReadLine());//объявляем переменные
+            double x, y, t;//объявляем переменные 
+            if (k > 0)//начало условия
             {
-                x = Math.Sin(a + (Math.Sqrt(a + k) / 1.5));
+                x = Math.Sin(a + (Math.Sqrt(a + k) / 1.5));//действие, если выполняется условие
             }
             else
-                x = Math.Cos(a * k) + 2.5 * a;
-            if (x <= 1) 
+                x = Math.Cos(a * k) + 2.5 * a;//действие, если условие не выполняется
+            if (x <= 1)//второе условие
             {
-                y = k + 5 * Math.Cos(Math.Abs(a) * x) + x;
+                y = k + 5 * Math.Cos(Math.Abs(a) * x) + x;//действие, если условие выполняется
             }
             else
-                y = 2 * a * x * x - 3 * k * x;
-            t = 1.3 * x * x + 1.7 * a * a - 3.1 * y * y;
+                y = 2 * a * x * x - 3 * k * x;//дйствие,если условие не выполняется
+            t = 1.3 * x * x + 1.7 * a * a - 3.1 * y * y;//вычисляем рузультат
             Console.WriteLine("Результат" + t);
         }
     }
---
+++ PZ_03/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PZ_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму покупки без скидки");
+            double sum = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите день недели");
+            string day = Console.ReadLine();
+            double discount;
+            switch (day)
+            {
+                case "Суббота":
+                    discount = 0.1;
+                    break;
+                case "Воскресенье":
+                    discount = 0.1;
+                    break;
+                default:
+                    discount = 0.05;
+                    break;
+            }
+            double discount1 = sum * discount;
+            double result = sum - discount1;
+            Console.WriteLine($"Скидка в этот день: {discount1 * 100}");
+            Console.WriteLine($"Сумма со скидкой: {result}");
+            Console.ReadLine();
+        }
+    }
+}
