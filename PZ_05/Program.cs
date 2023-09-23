cd9448f6223c07e4720ef1a7496ae4a2a6e0028c
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-09-23 15:02:51 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N: ");
+            int N = int.Parse(Console.ReadLine());
+            double doubleFactorial = 1;
+            if (N % 2 == 0)
+            {
+                int n = N;
+                do
+                {
+                    doubleFactorial *= n;
+                    n -= 2;
+                } while (n >= 2);
+            }
+            else
+            {
+                int n = N;
+                do
+                {
+                    doubleFactorial *= n;
+                    n -= 2;
+                } while (n >= 1);
+            }
+            Console.WriteLine("Двойной факториал числа N: " + doubleFactorial);
+        }
+    }
+}
