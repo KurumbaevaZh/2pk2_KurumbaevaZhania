7322e86a4760435626625648266de6a25191902e
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-11-19 14:14:18 +0500

    добавлена 9 практика

---
+++ PZ_09/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            // Разделение строки на слова по пробелам
+            string[] words = text.Split(' ');
+            int count = 0;
+            foreach (string word in words)
+            {
+                // Приведение слова к нижнему регистру и проверка первой буквы
+                if (word.ToLower().StartsWith("с"))
+                {
+                    count++;
+                }
+            }
+            // Вывод результата
+            Console.WriteLine("Количество слов на букву 'C': " + count);
+
+        }
+    }
+}
