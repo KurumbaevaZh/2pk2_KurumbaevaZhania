e498bb8be8030c54eaa27212862fbf798874c7ab
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-11-22 18:06:55 +0500

    добавлена 10 практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] sentences = text.Split('.'); // Разделяем текст на предложения
+            Array.Sort(sentences, (s1, s2) => s2.Split(' ').Length.CompareTo(s1.Split(' ').Length)); // Сортитурум предложения по убыванию количества слов в них
+            Console.WriteLine("Отсортированные предложения:"); // Вывод отсортированных предложений
+            foreach (string sentence in sentences)
+            {
+                Console.WriteLine(sentence.Trim() + ".");
+            }
+            Console.ReadKey();
+        }
+    }
+}
