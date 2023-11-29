73de2562f38517413946acf019dea56ec70b7025
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-11-29 21:28:10 +0500

    добавлена 12 практика

---
+++ PZ_12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                double[] array = { 4.5, 8.6, 9.3 }; 
+            int n = 5;
+                arrayFilter(array);
+            }
+            static double[] arrayFilter(double[] array)
+            {
+                double[] result = new double[0];
+                int count = 5; foreach (double i in array)
+                {
+                    if (i < 5)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                return result;
+            }
+    }
+}
