2bc10451210fc9e903ad09a39c4f57ac66b4e241
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-12-05 22:32:07 +0500

    добавлена 15 пратика

--- PZ_14/Program.cs
+++ PZ_14/Program.cs
@@ -8,14 +8,14 @@ namespace PZ_14
         {
             Console.WriteLine("Введите путь к файлу: ");
             string filePath=Console.ReadLine();
-            string text = File.ReadAllText(filePath);
-            string normalizedText = NormalizeText(text);
+            string text = File.ReadAllText(filePath); //чтение текста из файла
+            string normalizedText = NormalizeText(text); //нормализация текста
             Console.WriteLine(NormalizeText);
         }
         static string NormalizeText(string text)
         {
-            text = Regex.Replace(text, @"s+", " ");
-            text = Regex.Replace(text, @"(?<=[.!?])s*bw", m => m.Value.ToUpper()); 
+            text = Regex.Replace(text, @"s+", " "); //замена всех пробелов на один пробел
+            text = Regex.Replace(text, @"(?<=[.!?])s*bw", m => m.Value.ToUpper()); //первая буква текста заглавная
             return text;
         }
     }
---
+++ PZ_15/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите полный путь к каталогу:");
+            string directoryPath = Console.ReadLine();
+            string DirectoryPath1 = Path.Combine(directoryPath, "full");
+            string DirectoryPath2 = Path.Combine(directoryPath, "empty");
+            if (Directory.Exists(DirectoryPath1) && Directory.Exists(DirectoryPath2))
+            {
+                string[] files = Directory.GetFiles(DirectoryPath1);
+                int fileCount = files.Length;
+                int filesMoved = 0;
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string FilePath = Path.Combine(DirectoryPath2, fileName);
+                    File.Copy(file, FilePath, true);
+                    filesMoved++;
+                    Console.WriteLine($"Файл {filesMoved} из перенесен.");
+                }
+                Console.WriteLine("Перенос файлов завершен.");
+            }
+            else
+            {
+                Console.WriteLine("Каталог 'full' или 'empty' не существует.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
