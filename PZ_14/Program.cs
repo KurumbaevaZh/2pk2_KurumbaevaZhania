5a948b54c8207af7fbd49bc8cf2f76875e69d2fe
kurumbaevazania@gmail.com <kurumbaevazania@gmail.com>
2023-12-05 21:10:18 +0500

    добавлена 14 практика

---
+++ PZ_14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PZ_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к файлу: ");
+            string filePath=Console.ReadLine();
+            string text = File.ReadAllText(filePath);
+            string normalizedText = NormalizeText(text);
+            Console.WriteLine(NormalizeText);
+        }
+        static string NormalizeText(string text)
+        {
+            text = Regex.Replace(text, @"s+", " ");
+            text = Regex.Replace(text, @"(?<=[.!?])s*bw", m => m.Value.ToUpper()); 
+            return text;
+        }
+    }
+}
